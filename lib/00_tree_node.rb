865b163c6923a389f689fb2cc0f930b3e8c4de86
App Academy Student <aa@Apps-Mac-mini-26.local>
2019-08-16 13:07:32 -0400

    Complete parent= , add/remove child, dfs

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,50 @@
+require "byebug"
+
 class PolyTreeNode
 
-end
+  attr_reader :parent , :children, :value
+
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end 
+
+  def parent=(node)
+    if self.parent != nil
+      parent.children.reject!{|ele| ele == self}
+    end 
+    @parent = node   
+    if !self.parent.nil? && !node.children.include?(self)
+      node.children << self
+    end 
+  end 
+
+  def add_child(child_node)
+    child_node.parent=(self)
+  end 
+
+  def remove_child(child_node)
+    if self.children.include?(child_node)    
+      child_node.parent=(nil)
+    else 
+      raise "Error - Node is not a child" 
+    end 
+  end 
+
+def dfs(target_value)
+  # puts self.value 
+  # puts self.children[0].value
+  return self if self.value == target_value
+  if self.children.length >= 1 
+    (0...self.children.length).each do |i|
+      sub_tree = self.children
+      ans = sub_tree[i].dfs(target_value)
+      return ans unless ans.nil?
+    end
+  end 
+  nil
+end 
+
+
+end 
